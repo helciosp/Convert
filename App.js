b3778ec4e7947323775ad133ea17725b9ee60fcb
helciosp43 <helciosp43@gmail.com>
2021-08-18 17:25:18 -0300

    SrollView

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import Header  from './src/components/Header';
 import Base from './src/components/Base';
 
 export default function App() {
   return (
-    <View style={{flex: 1}}>
-      <StatusBar translucent= {false} backgroundColor='#1c1c1c' style='light' />
-      <Header />
-      <Base />
-    </View>
+    <ScrollView>
+      <View style={{flex: 1}}>
+        <StatusBar translucent= {false} backgroundColor='#1c1c1c' style='light' />
+        <Header />
+        <Base />
+      </View>
+    </ScrollView>
   );
 }
