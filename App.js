e6514f9ea2dc7c2ea32d33065400441d0a427739
helciosp43 <helciosp43@gmail.com>
2021-08-18 17:09:06 -0300

    App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
+import Header  from './src/components/Header';
 import Base from './src/components/Base';
 
 export default function App() {
   return (
     <View style={{flex: 1}}>
       <StatusBar translucent= {false} backgroundColor='#1c1c1c' style='light' />
+      <Header />
       <Base />
     </View>
   );
